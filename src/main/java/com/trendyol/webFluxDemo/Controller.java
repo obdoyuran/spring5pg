af42d61e8235a4166e1d4a340aa89a862b6e86e8
Osman Batur Doyuran <obdoyuran@gmail.com>
2018-11-06 23:57:29 +0300

    added sync call

--- src/main/java/com/trendyol/webFluxDemo/Controller.java
+++ src/main/java/com/trendyol/webFluxDemo/Controller.java
@@ -1,16 +1,25 @@
 package com.trendyol.webFluxDemo;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Flux;
 
 import java.time.Duration;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 @Slf4j
 @RestController
 public class Controller {
+
+    @Autowired
+    RestTemplate restTemplate;
 
     @GetMapping
     public Flux<BitbucketResponse> asyncCalls() {
@@ -21,9 +30,20 @@ public class Controller {
                             .get()
                             .retrieve()
                             .bodyToMono(BitbucketResponse.class)
-                            .delayElement(Duration.ofSeconds(3))
-                            .log()
-                            .doOnSuccess(r -> log.info("Finished {}", i));
+                            .doOnNext(r -> log.info("Finished {}", i));
                 });
+    }
+
+    @GetMapping("/sync")
+    public List<BitbucketResponse> syncCalls() {
+        return IntStream.range(0, 3)
+                .boxed()
+                .map(i -> {
+                    log.info("Started {}", i);
+                    BitbucketResponse response = restTemplate.getForObject("https://bqlf8qjztdtr.statuspage.io/api/v2/status.json", BitbucketResponse.class);
+                    log.info("Finished {}", i);
+                    return response;
+                })
+                .collect(Collectors.toList());
     }
 }
--- src/main/java/com/trendyol/webFluxDemo/WebFluxDemoApplication.java
+++ src/main/java/com/trendyol/webFluxDemo/WebFluxDemoApplication.java
@@ -2,6 +2,8 @@ package com.trendyol.webFluxDemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.reactive.config.EnableWebFlux;
 
 @SpringBootApplication
@@ -10,5 +12,10 @@ public class WebFluxDemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WebFluxDemoApplication.class, args);
+	}
+
+	@Bean
+	public RestTemplate getRestTemplate() {
+		return new RestTemplate();
 	}
 }
